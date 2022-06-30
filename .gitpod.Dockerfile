606354620c07ba89690378c19d637457405e84ec
Tousif <tousif101@outlook.com>
2022-07-01 01:08:46 +0530

    fix: update docker compose (#2)
    * fix: update docker compose
    
    * feat: added reviewdog
    
    * fix: some packages added

--- .gitpod.Dockerfile
+++ .gitpod.Dockerfile
@@ -3,9 +3,9 @@ FROM gitpod/workspace-full
 RUN npm i --location=global @nestjs/cli tsx serve fkill-cli
 
 # Install kubectl, k3d, tilt
-RUN brew install fzf navi git-secret kubectl k3d starship dbmate
+RUN brew install fzf navi git-secret kubectl k3d starship dbmate go-task/tap/go-task
 
-RUN eval "$(starship init bash)"
+# RUN eval "$(starship init bash)"
 # k3d cluster create gitpod
 
 # RUN curl -fsSL https://raw.githubusercontent.com/tilt-dev/tilt/master/scripts/install.sh | bash
--- apps/notifier/src/app.module.ts
+++ apps/notifier/src/app.module.ts
@@ -1,10 +1,12 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { EnvalidModule } from "nestjs-envalid";
+import { validators } from "./config";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [EnvalidModule.forRoot({ validators })],
+    controllers: [AppController],
+    providers: [AppService],
 })
 export class AppModule {}
