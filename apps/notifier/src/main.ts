407f6e10383561f9766d1d4757ada07b670d706a
Tousif <tousif101@outlook.com>
2022-06-30 20:35:03 +0530

    feat: adding notifier microservice (#1)
    * feat: adding notifier microservice
    
    * fix: update compose

---
+++ apps/notifier/src/main.ts
@@ -0,0 +1,18 @@
+import { NestFactory } from '@nestjs/core';
+import { Transport, MicroserviceOptions } from '@nestjs/microservices';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.NATS,
+      options: {
+        servers: ['nats://localhost:4222'],
+      },
+    },
+  );
+
+  await app.listen();
+}
+bootstrap();
