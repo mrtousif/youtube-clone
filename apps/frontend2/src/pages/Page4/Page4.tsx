8a7d8142974e2ca305f2e795304ec13dd1d5c092
Tousif <tousif101@outlook.com>
2022-07-02 22:52:11 +0530

    chore: idk

--- apps/frontend2/src/pages/Page4/Page4.tsx
+++ apps/frontend2/src/pages/Page4/Page4.tsx
@@ -7,24 +7,24 @@ import Meta from '@/components/Meta';
 import { FullSizeCenteredFlexBox } from '@/components/styled';
 
 function Page4() {
-  return (
-    <>
-      <Meta title="page 4" />
-      <FullSizeCenteredFlexBox flexDirection="column">
-        <Typography variant="h3">Page 4</Typography>
-        <Button
-          to={`/${Math.random().toString()}`}
-          component={Link}
-          variant="outlined"
-          sx={{ mt: 4 }}
-          size="small"
-          color="warning"
-        >
-          Whant to check 404?
-        </Button>
-      </FullSizeCenteredFlexBox>
-    </>
-  );
+    return (
+        <>
+            <Meta title="page 4" />
+            <FullSizeCenteredFlexBox flexDirection="column">
+                <Typography variant="h3">Page 4</Typography>
+                <Button
+                    to={`/${Math.random().toString()}`}
+                    component={Link}
+                    variant="outlined"
+                    sx={{ mt: 4 }}
+                    size="small"
+                    color="warning"
+                >
+                    Want to check 404?
+                </Button>
+            </FullSizeCenteredFlexBox>
+        </>
+    );
 }
 
 export default Page4;
