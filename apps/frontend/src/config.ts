604343cd64271d7eb536acca281be51cb677674e
Tousif <tousif101@outlook.com>
2022-07-02 23:26:59 +0530

    chore: delete old frontend

--- apps/frontend/src/config.ts
+++
@@ -1,7 +0,0 @@
-const env = {
-    NODE_ENV: process.env.NODE_ENV || "development",
-    isProduction: process.env.NODE_ENV === "production",
-    isDev: process.env.NODE_ENV === "development",
-};
-
-export default env;
