571a4d3df6e5ae83e749da97a52470bd8a4edaa0
Tousif <tousif101@outlook.com>
2022-07-09 10:41:06 +0530

    chore: adding realtime & keyclaok container

--- init/init-user-db.sh
+++ init/init-user-db.sh
@@ -5,4 +5,8 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-E
     CREATE USER hasura WITH PASSWORD 'postgres';
     CREATE DATABASE hasura_metadata;
     GRANT ALL PRIVILEGES ON DATABASE hasura_metadata TO hasura;
+
+    CREATE USER keycloak WITH PASSWORD 'password';
+    CREATE DATABASE keycloak_db;
+    GRANT ALL PRIVILEGES ON DATABASE keycloak_db TO keycloak;
 EOSQL
