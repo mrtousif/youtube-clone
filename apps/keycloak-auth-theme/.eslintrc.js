5d3b6d14e2867988736ed8386e5423dcedc589b5
Tousif <tousif101@outlook.com>
2022-07-16 09:42:31 +0530

    feat: adding new poroject keycloak auth theme

---
+++ apps/keycloak-auth-theme/.eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+    root: true,
+    extends: ['custom'],
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    settings: {},
+    // "parserOptions": {
+    //   "ecmaFeatures": {
+    //     "jsx": true
+    //   },
+    //   "ecmaVersion": "latest",
+    //   "sourceType": "module"
+    // }
+};
---
+++ apps/keycloak-auth-theme/src/KcApp/kcMessagesExtension.ts
@@ -0,0 +1,11 @@
+import { kcMessages } from 'keycloakify';
+
+Object.assign(kcMessages['fr'], {
+    /* spell-checker: disable */
+    gender: 'Genre',
+    /* spell-checker: enable */
+});
+
+Object.assign(kcMessages['en'], {
+    gender: 'Gender',
+});
