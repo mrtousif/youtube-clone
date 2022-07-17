3d171946f77be08ce76250cc9efd5e53d6038021
CoderTousif <tousif101@outlook.com>
2022-07-17 10:59:56 +0000

    chore: start up

---
+++ init/init-extensions.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+    CREATE EXTENSION IF NOT EXISTS citext;
+
+EOSQL
