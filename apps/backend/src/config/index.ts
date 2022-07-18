b1c6af8a03ea265fce7e513aac34d4c78a709f96
CoderTousif <tousif101@outlook.com>
2022-07-18 17:08:40 +0000

    chore: update setup

--- apps/backend/src/config/index.ts
+++ apps/backend/src/config/index.ts
@@ -1,4 +1,4 @@
-import { makeValidators, num, str, Static } from 'nestjs-envalid';
+import { makeValidators, num, str,url, Static } from 'nestjs-envalid';
 
 export const validators = makeValidators({
     NODE_ENV: str({ choices: ['development', 'test', 'production', 'staging'] }),
@@ -6,7 +6,7 @@ export const validators = makeValidators({
     NESTJS_EVENT_WEBHOOK_SHARED_SECRET: str(),
     HASURA_GRAPHQL_API_ENDPOINT: str(),
     S3_REGION: str(),
-    S3_ENDPOINT: str(),
+    S3_ENDPOINT: url(),
     S3_BUCKET: str(),
 });
 
