2047ce6ff332664c1453308fd12fe74181841e94
CoderTousif <tousif101@outlook.com>
2022-07-19 19:22:25 +0000

    chore: update cong

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -e
+export PGPASSWORD="$POSTGRES_PASSWORD";
 
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+psql -v ON_ERROR_STOP=1 -h localhost  --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
     CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
     CREATE EXTENSION IF NOT EXISTS citext;
 
