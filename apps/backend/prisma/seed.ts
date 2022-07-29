ab2edb2c20cba42fe754358b3471c470e7b749aa
CoderTousif <tousif101@outlook.com>
2022-07-29 18:12:59 +0000

    chore: update

--- apps/backend/prisma/seed.ts
+++ apps/backend/prisma/seed.ts
@@ -1,26 +1,37 @@
-import { PrismaClient } from '@prisma/client'
 import { faker } from '@faker-js/faker';
+import { PrismaClient } from '@prisma/client';
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
 async function main() {
-  const newUser = [...Array(5).keys()].map(async _item => await prisma.users.create({
-    data: {
-      email: faker.internet.email(),
-      name: faker.name.findName(),
-      phone: faker.phone.number(),
-      channel_name: faker.name.findName()
-    }
-  }))
+    [...Array(5).keys()].map(async (_item) => {
+        const newUser = await prisma.users.create({
+            data: {
+                email: faker.internet.email(),
+                name: faker.name.findName(),
+                phone: faker.phone.number(),
+                channel_name: faker.random.word(),
+                description: faker.random.words(5),
+            },
+        });
 
+        await prisma.videos.create({
+            data: {
+                stream_url: faker.internet.url(),
+                channel_id: newUser.id,
+                thumbnail: faker.image.nature(),
+                title: faker.random.words(5),
+            },
+        });
+    });
 }
 
 main()
-  .then(async () => {
-    await prisma.$disconnect()
-  })
-  .catch(async (e) => {
-    console.error(e)
-    await prisma.$disconnect()
-    process.exit(1)
-  })
+    .then(async () => {
+        await prisma.$disconnect();
+    })
+    .catch(async (e) => {
+        console.error(e);
+        await prisma.$disconnect();
+        process.exit(1);
+    });
