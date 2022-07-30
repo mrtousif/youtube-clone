7e669f45e9bfeb2f14327a4ee3714950f4d90a7b
Tousif <tousif101@outlook.com>
2022-07-30 21:42:10 +0530

    feat: added dockefile for backend

---
+++ apps/backend/.pnpmfile.cjs
@@ -0,0 +1,19 @@
+// ./docker/.pnpmfile.cjs
+// https://github.com/pnpm/pnpm/issues/4378
+//
+
+module.exports = {
+    hooks: {
+        readPackage(pkg) {
+            pkg.dependenciesMeta = pkg.dependenciesMeta || {};
+            for (const [depName, depVersion] of Object.entries(pkg.dependencies)) {
+                if (depVersion.startsWith('workspace:')) {
+                    pkg.dependenciesMeta[depName] = {
+                        injected: true,
+                    };
+                }
+            }
+            return pkg;
+        },
+    },
+};
