8792c4acca4339b3fe48d5fd04ceedf1268ece20
CoderTousif <tousif101@outlook.com>
2022-08-07 16:29:35 +0000

    chore: update prisma seed

--- apps/backend/prisma/seed.ts
+++ apps/backend/prisma/seed.ts
@@ -5,22 +5,27 @@ const prisma = new PrismaClient();
 
 async function main() {
     [...Array(5).keys()].map(async (_item) => {
-        const newUser = await prisma.users.create({
+        await prisma.users.create({
             data: {
                 email: faker.internet.email(),
                 name: faker.name.findName(),
                 phone: faker.phone.number(),
                 channel_name: faker.random.word(),
                 description: faker.random.words(5),
-            },
-        });
-
-        await prisma.videos.create({
-            data: {
-                stream_url: faker.internet.url(),
-                channel_id: newUser.id,
-                thumbnail: faker.image.nature(),
-                title: faker.random.words(5),
+                videos: {
+                    create: [
+                        {
+                            stream_url: faker.internet.url(),
+                            thumbnail: faker.image.nature(),
+                            title: faker.random.words(5),
+                        },
+                        {
+                            stream_url: faker.internet.url(),
+                            thumbnail: faker.image.nature(),
+                            title: faker.random.words(5),
+                        },
+                    ],
+                },
             },
         });
     });
