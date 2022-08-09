cc8f3d96f515288521221c86a84f7e5e297356e0
CoderTousif <tousif101@outlook.com>
2022-08-09 19:03:26 +0000

    feat: added redis middleware for prisma

--- apps/backend/src/PrismaConfigService.ts
+++ apps/backend/src/PrismaConfigService.ts
@@ -1,22 +1,45 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaOptionsFactory, PrismaServiceOptions } from 'nestjs-prisma';
-// import { ENVALID, EnvalidModule } from 'nestjs-envalid';
-// import { Config } from './config';
-// import { Prisma } from '@prisma/client';
+import { ENVALID, EnvalidModule } from 'nestjs-envalid';
+import { Config } from './config';
+import { createPrismaRedisCache } from "prisma-redis-middleware";
+import Redis from "ioredis";
 
-// export function loggingMiddleware(): Prisma.Middleware {
-//     return async (params, next) => {
-//         const before = Date.now();
+const redis = new Redis();
 
-//         const result = await next(params);
+export function loggingMiddleware() {
+    return async (params, next) => {
+        const before = Date.now();
 
-//         const after = Date.now();
+        const result = await next(params);
 
-//         console.log(`Query ${params.model}.${params.action} took ${after - before}ms`);
+        const after = Date.now();
 
-//         return result;
-//     };
-// }
+        console.log(`Query ${params.model}.${params.action} took ${after - before}ms`);
+
+        return result;
+    };
+}
+
+const cacheMiddleware = () => createPrismaRedisCache({
+    models: [
+      { model: "User", excludeMethods: ["findMany"] },
+      { model: "Video", cacheTime: 180, cacheKey: "article" },
+    ],
+    storage: { type: "redis", options: { client: redis, invalidation: { referencesTTL: 300 }, log: console } },
+    cacheTime: 300,
+    excludeModels: ["Product", "Cart"],
+    excludeMethods: ["count", "groupBy"],
+    onHit: (key) => {
+      console.log("hit", key);
+    },
+    onMiss: (key) => {
+      console.log("miss", key);
+    },
+    onError: (key) => {
+      console.log("error", key);
+    },
+  })
 
 @Injectable()
 export class PrismaConfigService implements PrismaOptionsFactory {
@@ -30,7 +53,7 @@ export class PrismaConfigService implements PrismaOptionsFactory {
                 log: ['info', 'query'],
             },
             explicitConnect: true,
-            // middlewares: [loggingMiddleware()],
+            middlewares: [loggingMiddleware(), cacheMiddleware()],
         };
     }
 }
