900b20f6a3fda548c0de30a41766f594d6e6c3cb
CoderTousif <tousif101@outlook.com>
2022-08-12 15:41:40 +0000

    chore: setting up logger

--- apps/backend/src/PrismaConfigService.ts
+++ apps/backend/src/PrismaConfigService.ts
@@ -42,10 +42,6 @@ const cacheMiddleware = () => createPrismaRedisCache({
 
 @Injectable()
 export class PrismaConfigService implements PrismaOptionsFactory {
-    constructor() {
-        // TODO inject any other service here like the `ConfigService`
-    }
-
     createPrismaOptions(): PrismaServiceOptions | Promise<PrismaServiceOptions> {
         return {
             prismaOptions: {
--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,29 +1,20 @@
 import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
 import { ClsMiddleware } from 'nestjs-cls';
+import { LoggerErrorInterceptor, Logger } from 'nestjs-pino';
 import { PrismaClientExceptionFilter, PrismaService } from 'nestjs-prisma';
-import { config } from './config';
-// import underPressure from "@fastify/under-pressure";
 import { AppModule } from './app.module';
-
-declare const module: any;
+import { config } from './config';
 
 async function bootstrap() {
     const app = await NestFactory.create<NestFastifyApplication>(
         AppModule,
         new FastifyAdapter({
-            logger: {
-                prettyPrint: config.isDevelopment,
-            },
             trustProxy: true,
-        })
+        }),
+        { bufferLogs: true }
     );
-    // app.register(underPressure, {
-    //     maxEventLoopDelay: 1000,
-    //     maxHeapUsedBytes: 100000000,
-    //     maxRssBytes: 100000000,
-    //     maxEventLoopUtilization:0.98
-    // });
+    
     app.use(
         new ClsMiddleware({
             useEnterWith: true,
@@ -34,13 +25,14 @@ async function bootstrap() {
     prismaService.enableShutdownHooks(app);
 
     const { httpAdapter } = app.get(HttpAdapterHost);
+
     app.useGlobalFilters(new PrismaClientExceptionFilter(httpAdapter));
 
-    await app.listen(config.PORT);
-
-    if (module.hot) {
-        module.hot.accept();
-        module.hot.dispose(() => app.close());
+    if(config.isProduction){
+        app.useLogger(app.get(Logger));
+        app.useGlobalInterceptors(new LoggerErrorInterceptor());
     }
+
+    await app.listen(config.PORT);
 }
 bootstrap();
