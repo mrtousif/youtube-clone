e9f9382577f4a2f20528c750d31ff9b9c7ad5774
CoderTousif <tousif101@outlook.com>
2022-08-20 05:55:39 +0000

    chore(rect-admin): update

--- apps/admin-panel/src/App.tsx
+++ apps/admin-panel/src/App.tsx
@@ -1,3 +1,4 @@
+// import { useState, useEffect } from 'react'
 import CssBaseline from '@mui/material/CssBaseline';
 import { withErrorHandler } from '@/error-handling';
 import AppErrorBoundaryFallback from '@/error-handling/fallbacks/App';
@@ -9,12 +10,23 @@ import jsonServerProvider from "ra-data-json-server";
 // import buildHasuraProvider from 'ra-data-hasura';
 // import { client } from '@/graphql';
 
-// const hasuraDataProvider = await buildHasuraProvider({
-//     client,
-// });
 const dataProvider = jsonServerProvider("https://jsonplaceholder.typicode.com");
 
 function App() {
+    // const [dataProvider, setDataProvider] = useState(null);
+
+    // useEffect(() => {
+    //     const buildDataProvider = async () => {
+    //     const dataProvider = await buildHasuraProvider({
+    //             client,
+    //         });
+    //     setDataProvider(() => dataProvider);
+    //     };
+    //     buildDataProvider();
+    // }, []);
+
+    // if (!dataProvider) return <p>Loading...</p>;
+
     return (
         <>
             <CssBaseline />
