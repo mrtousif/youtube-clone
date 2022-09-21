268b155b646ae95a937f5b691740074acc4748de
CoderTousif <tousif101@outlook.com>
2022-09-21 20:10:48 +0000

    chore: update

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -4,21 +4,8 @@ import { Transport } from '@nestjs/microservices';
 import { ClsMiddleware } from 'nestjs-cls';
 import { LoggerErrorInterceptor, Logger } from 'nestjs-pino';
 import { PrismaClientExceptionFilter, PrismaService } from 'nestjs-prisma';
-import keycloak, { KeycloakOptions, UserInfo } from 'fastify-keycloak-adapter'
 import { AppModule } from './app.module';
 import { config } from './config';
-
-const opts: KeycloakOptions = {
-    appOrigin: 'http://localhost:6000',
-    keycloakSubdomain: 'keycloak.mycompany.com/auth/realms/myrealm',
-    useHttps: !config.isDev,
-    autoRefreshToken: true,
-    clientId: 'myclient01',
-    clientSecret: 'myClientSecret',
-    logoutEndpoint: '/logout',
-    excludedPatterns: ['/metrics', '/manifest.json', '/api/todos/**'],
-    callback: '/callback'
-}
 
 async function bootstrap() {
     const app = await NestFactory.create<NestFastifyApplication>(
@@ -44,7 +31,6 @@ async function bootstrap() {
             useEnterWith: true,
         }).use
     );
-    app.register(keycloak, opts)
 
     const prismaService: PrismaService = app.get(PrismaService);
     prismaService.enableShutdownHooks(app);
