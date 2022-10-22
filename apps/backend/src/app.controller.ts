a07779494052029ad993f79a8e0df7e5315b76e1
CoderTousif <tousif101@outlook.com>
2022-10-22 15:35:58 +0000

    fix: update

--- apps/backend/src/app.controller.ts
+++ apps/backend/src/app.controller.ts
@@ -1,12 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Request } from '@nestjs/common';
+import type { FastifyRequest } from 'fastify';
+
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+    constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
+    @Get()
+    getHello(@Request() req: FastifyRequest): string {
+        if (req.user) {
+            const visits = req.session.get('visits');
+            return 'Hello, ' + req.user.userinfo.name + '! <a href="/logout">Logout</a>';
+        } else {
+            return this.appService.getHello() + ' <a href="/login">Login</a>';
+        }
+    }
 }
