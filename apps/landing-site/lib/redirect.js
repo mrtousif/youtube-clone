47fddef5e8c1be3c7da7308d00c5ccd94eabf168
CoderTousif <tousif101@outlook.com>
2022-10-24 12:53:52 +0000

    fix: next lint build

--- apps/landing-site/lib/redirect.js
+++ apps/landing-site/lib/redirect.js
@@ -5,18 +5,18 @@ import languageDetector from './languageDetector'
 
 export const useRedirect = (to) => {
   const router = useRouter()
-  to = to || router.asPath
+  const redirectTo = to || router.asPath
 
   // language detection
   useEffect(() => {
     const detectedLng = languageDetector.detect()
-    if (to.startsWith('/' + detectedLng) && router.route === '/404') { // prevent endless loop
+    if (redirectTo.startsWith('/' + detectedLng) && router.route === '/404') { // prevent endless loop
       router.replace('/' + detectedLng + router.route)
       return
     }
 
     languageDetector.cache(detectedLng)
-    router.replace('/' + detectedLng + to)
+    router.replace('/' + detectedLng + redirectTo)
   })
 
   return <></>
