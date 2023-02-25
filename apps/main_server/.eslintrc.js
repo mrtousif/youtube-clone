700ba6390b229fa647288ba0417ba50e74dda1ec
Tousif <tousif101@outlook.com>
2023-02-25 20:57:07 +0530

    Backport (#126)
    * ci: backport update
    
    * ci: update
    
    * ci: update
    
    * ci: update
    
    * ci: update
    
    * ci: update
    
    * feat: adding vitest preview and safeql
    
    * refactor: update

--- apps/main_server/.eslintrc.js
+++ apps/main_server/.eslintrc.js
@@ -1,9 +1,27 @@
 module.exports = {
     root: true,
     extends: ['custom'],
-    ignorePatterns: ['**/sdk.ts', "**/global-*.js"],
+    ignorePatterns: ['**/sdk.ts', '**/global-*.js'],
     env: {
         node: true,
         jest: true,
+    },
+    rules: {
+        '@ts-safeql/check-sql': [
+            'error',
+            {
+                connections: [
+                    {
+                        // The URL of the database:
+                        databaseUrl: 'postgres://postgres:postgres@localhost:5432/my_database',
+                        // The name of the variable that holds the connection:
+                        name: 'myDb',
+                        // An array of operators that wraps the raw query:
+                        operators: ['rawQuery'],
+                        migrationsDir: './db/migrations',
+                    },
+                ],
+            },
+        ],
     },
 };
