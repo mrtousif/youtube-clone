f72e5386452da9fb7590e520bc991bc929f92e54
CoderTousif <tousif101@outlook.com>
2023-07-10 07:29:13 +0000

    feat(audit): add pgmemento

---
+++ apps/main_server/db/migrations/20230710072118_pgmemento.sql
@@ -0,0 +1,15 @@
+-- migrate:up
+CREATE EXTENSION IF NOT EXISTS pgmemento;
+
+SELECT pgmemento.init(
+  schemaname := 'public',                     -- default is 'public' 
+  audit_id_column_name := 'pgmemento_audit_id',  -- default is 'pgmemento_audit_id'
+  log_old_data := TRUE,                      -- default is true
+  log_new_data := TRUE,                      -- default is false
+  log_state := FALSE,                         -- default is false
+  trigger_create_table := FALSE,               -- default is false
+  except_tables  := ARRAY['schema_migrations', 'channel_subscriptions']       -- default is empty
+);
+
+-- migrate:down
+DROP EXTENSION IF EXISTS pgmemento
