16751c9c7d1f24f3abd279ab5d929d5f39bf0161
Tousif <tousif101@outlook.com>
2023-07-14 21:42:58 +0530

    feat: add k8s yaml

--- apps/main_server/db/migrations/20230710072118_pgmemento.sql
+++ apps/main_server/db/migrations/20230710072118_pgmemento.sql
@@ -5,8 +5,8 @@ SELECT pgmemento.init(
   schemaname := 'public',                     -- default is 'public' 
   audit_id_column_name := 'pgmemento_audit_id',  -- default is 'pgmemento_audit_id'
   log_old_data := TRUE,                      -- default is true
-  log_new_data := TRUE,                      -- default is false
-  log_state := FALSE,                         -- default is false
+  log_new_data := FALSE,                      -- default is false
+  log_state := TRUE,                         -- default is false
   trigger_create_table := FALSE,               -- default is false
   except_tables  := ARRAY['schema_migrations', 'channel_subscriptions']       -- default is empty
 );
